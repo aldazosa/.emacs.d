1e11f57419a2a0af420d9f3293231c92712814db
Mauricio Aldazosa Mariaca <mauricio.aldazosa@ciencias.unam.mx>
2013-03-02 16:17:44 -0600

    Finishes paredit setup.

--- setup-paredit.el
+++ setup-paredit.el
@@ -1,8 +1,20 @@
-(autoload 'paredit-mode "paredit"
-  "Minor mode for pseudo-structurally editing Lisp code." t)
+(require 'paredit)
+
 (add-hook 'emacs-lisp-mode-hook       (lambda () (paredit-mode +1)))
 (add-hook 'lisp-mode-hook             (lambda () (paredit-mode +1)))
 (add-hook 'lisp-interaction-mode-hook (lambda () (paredit-mode +1)))
 (add-hook 'clojure-mode-hook          (lambda () (paredit-mode +1)))
+
+(defun paredit-wrap-round-from-behind ()
+  (interactive)
+  (forward-sexp -1)
+  (paredit-wrap-round)
+  (insert " ")
+  (forward-char -1))
+
+(define-key paredit-mode-map (kbd "M-)")
+  'paredit-wrap-round-from-behind)
+
+(diminish 'paredit-mode " π")
 
 (provide 'setup-paredit)
