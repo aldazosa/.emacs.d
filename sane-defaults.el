73f8011c39bf7f8f499e9d26c3b71929f20800a2
Mauricio Aldazosa Mariaca <mauricio.aldazosa@ciencias.unam.mx>
2013-03-07 23:20:18 -0600

    Configure Autosave.

--- sane-defaults.el
+++ sane-defaults.el
@@ -111,6 +111,15 @@
 ;; New line at EOF
 (setq require-final-newline t)
 
+;; Write backup files to own directory
+(setq backup-directory-alist
+      `(("." . ,(expand-file-name
+                 (concat user-emacs-directory "backups")))))
+
+;; Make backups of files, even when they're in version control
+(setq vc-make-backup-files t)
+
+
 ;; No arrow navigation
 (global-set-key (kbd "<left>") (lambda ()
                                  (interactive)
