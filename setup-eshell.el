ff5c22b3aa10e8656b22d606f5dc5e8b4412abdd
Mauricio Aldazosa Mariaca <mauricio.aldazosa@ciencias.unam.mx>
2013-03-08 09:33:46 -0600

    Customizes eshell.

--- setup-eshell.el
+++ setup-eshell.el
@@ -1,52 +1,54 @@
-(add-hook 'eshell-mode-hook (lambda () (set-face-foreground 'eshell-prompt "turquoise4")))
+(add-hook 'eshell-mode-hook (lambda ()
+                              (set-face-foreground 'eshell-prompt "turquoise4")
+                              (electric-pair-mode)))
 
 ;; From https://github.com/jimm/elisp/blob/master/eshell-customize.el
 
-(defun pwd-repl-home (pwd)
-  (interactive)
-  (let* ((home (expand-file-name (getenv "HOME")))
-   (home-len (length home)))
-    (if (and
-   (>= (length pwd) home-len)
-   (equal home (substring pwd 0 home-len)))
-  (concat "~" (substring pwd home-len))
-      pwd)))
+;; (defun pwd-repl-home (pwd)
+;;   (interactive)
+;;   (let* ((home (expand-file-name (getenv "HOME")))
+;;    (home-len (length home)))
+;;     (if (and
+;;    (>= (length pwd) home-len)
+;;    (equal home (substring pwd 0 home-len)))
+;;   (concat "~" (substring pwd home-len))
+;;       pwd)))
 
-(defun curr-dir-git-branch-string (pwd)
-  "Returns current git branch as a string, or the empty string if
-PWD is not in a git repo (or the git command is not found)."
-  (interactive)
-  (when (and (eshell-search-path "git")
-             (locate-dominating-file pwd ".git"))
-    (let ((git-output (shell-command-to-string (concat "cd " pwd " && git branch | grep '\\*' | sed -e 's/^\\* //'"))))
-      (concat " ["
-              (if (> (length git-output) 0)
-                  (substring git-output 0 -1)
-                "(no branch)")
-              "] ")
-      )))
+;; (defun curr-dir-git-branch-string (pwd)
+;;   "Returns current git branch as a string, or the empty string if
+;; PWD is not in a git repo (or the git command is not found)."
+;;   (interactive)
+;;   (when (and (eshell-search-path "git")
+;;              (locate-dominating-file pwd ".git"))
+;;     (let ((git-output (shell-command-to-string (concat "cd " pwd " && git branch | grep '\\*' | sed -e 's/^\\* //'"))))
+;;       (concat " ["
+;;               (if (> (length git-output) 0)
+;;                   (substring git-output 0 -1)
+;;                 "(no branch)")
+;;               "] ")
+;;       )))
 
-(setq eshell-prompt-function
-      (lambda ()
-        (concat
-         (propertize ((lambda (p-lst)
-            (if (> (length p-lst) 3)
-                (concat
-                 (mapconcat (lambda (elm) (if (zerop (length elm)) ""
-                                            (substring elm 0 1)))
-                            (butlast p-lst 3)
-                            "/")
-                 "/"
-                 (mapconcat (lambda (elm) elm)
-                            (last p-lst 3)
-                            "/"))
-              (mapconcat (lambda (elm) elm)
-                         p-lst
-                         "/")))
-          (split-string (pwd-repl-home (eshell/pwd)) "/")) 'face `(:foreground "turquoise4"))
-         (or (curr-dir-git-branch-string (eshell/pwd)))
-         (propertize "# " 'face 'default))))
+;; (setq eshell-prompt-function
+;;       (lambda ()
+;;         (concat
+;;          (propertize ((lambda (p-lst)
+;;             (if (> (length p-lst) 3)
+;;                 (concat
+;;                  (mapconcat (lambda (elm) (if (zerop (length elm)) ""
+;;                                             (substring elm 0 1)))
+;;                             (butlast p-lst 3)
+;;                             "/")
+;;                  "/"
+;;                  (mapconcat (lambda (elm) elm)
+;;                             (last p-lst 3)
+;;                             "/"))
+;;               (mapconcat (lambda (elm) elm)
+;;                          p-lst
+;;                          "/")))
+;;           (split-string (pwd-repl-home (eshell/pwd)) "/")) 'face `(:foreground "turquoise4"))
+;;          (or (curr-dir-git-branch-string (eshell/pwd)))
+;;          (propertize "# " 'face 'default))))
 
-(setq eshell-highlight-prompt nil)
+;; (setq eshell-highlight-prompt nil)
 
 (provide 'setup-eshell)
