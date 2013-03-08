db38853d7e7543572ccf3c81ebdcfea8afef7722
Mauricio Aldazosa Mariaca <mauricio.aldazosa@ciencias.unam.mx>
2013-03-08 11:58:06 -0600

    Sets which-func face.

--- custom.el
+++ custom.el
@@ -14,8 +14,8 @@
  '(magit-branch ((t (:foreground "#C6B032"))))
  '(magit-item-highlight ((t (:background "gray12"))))
  '(magit-section-title ((t (:foreground "#46657B"))))
- '(org-done ((t (:foreground "dark green" :weight bold))))
- '(org-todo ((t (:foreground "red2" :weight bold))))
+ '(org-done ((t (:foreground "dark green" :weight bold))) t)
+ '(org-todo ((t (:foreground "red2" :weight bold))) t)
  '(rainbow-delimiters-depth-1-face ((t (:foreground "#93e0e3"))))
  '(rainbow-delimiters-depth-2-face ((t (:foreground "#f0dfaf"))))
  '(rainbow-delimiters-depth-3-face ((t (:foreground "#94bff3"))))
@@ -25,7 +25,8 @@
  '(rainbow-delimiters-depth-7-face ((t (:foreground "#bfebbf"))))
  '(rainbow-delimiters-depth-8-face ((t (:foreground "#9c6363"))))
  '(rainbow-delimiters-depth-9-face ((t (:foreground "#d0bf8f"))))
- '(rainbow-delimiters-unmatched-face ((t (:foreground "#88090B")))))
+ '(rainbow-delimiters-unmatched-face ((t (:foreground "#88090B"))))
+ '(which-func ((t (:foreground "sky blue")))))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
