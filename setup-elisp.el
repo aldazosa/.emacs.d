94da5b5776b5eeb4ddfd0b847f7344de1745e6e7
Mauricio Aldazosa Mariaca <mauricio.aldazosa@ciencias.unam.mx>
2013-03-07 23:19:41 -0600

    Autocompile emacs lisp files on save.

--- setup-elisp.el
+++ setup-elisp.el
@@ -1,7 +1,11 @@
-
 (require 'setup-lisp)
 
 (add-hook 'emacs-lisp-mode-hook (lambda()
                                   (lisp-defaults)
                                   (turn-on-eldoc-mode)))
+(add-hook 'after-save-hook
+          (lambda ()
+            (if (eq major-mode 'emacs-lisp-mode)
+                (save-excursion (byte-compile-file buffer-file-name)))))
+
 (provide 'setup-elisp)
