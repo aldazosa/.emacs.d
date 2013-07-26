832788dbe3845881192e2926b29df40887f65810
Mauricio Aldazosa Mariaca <mauricio.aldazosa@ciencias.unam.mx>
2013-07-26 11:03:47 -0500

    Adds function to jump between java and Tapestry's tml files.

--- setup-java-mode.el
+++ setup-java-mode.el
@@ -37,4 +37,21 @@
 
 (global-set-key (kbd "<f6>") 'run-mvn)
 
+(defun switch-to-tml (java-file)
+  (interactive)
+  (find-file (replace-regexp-in-string "\\(.*\\)src/main/java\\(.*\\)/\\(.*\\).java" "\\1src/main/resources\\2/\\3.tml" java-file)))
+
+(defun switch-to-java (tml-file)
+  (interactive)
+  (find-file (replace-regexp-in-string "\\(.*\\)src/main/resources\\(.*\\)/\\(.*\\).tml" "\\1src/main/java\\2/\\3.java" tml-file)))
+
+(defun switch-to-companion-file ()
+  "switch from a .java file to its .tml companion and viceversa"
+  (interactive)
+  (let ((buffer-file (buffer-file-name)))
+    (cond ((string-match "\.java$" buffer-file) (switch-to-tml buffer-file))
+          ((string-match "\.tml$" buffer-file) (switch-to-java buffer-file)))))
+
+(global-set-key (kbd "C-x C-j") 'switch-to-companion-file)
+
 (provide 'setup-java-mode)
