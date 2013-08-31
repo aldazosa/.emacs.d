e93ce39b3f7286a5d0dfaf3c431897db4148711b
Mauricio Aldazosa Mariaca <mauricio.aldazosa@ciencias.unam.mx>
2013-08-31 09:34:10 -0500

    Adds a fun to prettify xml.

--- defuns.el
+++ defuns.el
@@ -23,3 +23,19 @@
       (message "No face at %d" pos))))
 
 (provide 'defuns)
+;; Taken from
+;; http://blog.bookworm.at/2007/03/pretty-print-xml-with-emacs.html
+(defun pretty-print-xml-region (begin end)
+  "Pretty format XML markup in region. You need to have nxml-mode
+http://www.emacswiki.org/cgi-bin/wiki/NxmlMode installed to do
+this.  The function inserts linebreaks to separate tags that have
+nothing but whitespace between them.  It then indents the markup
+by using nxml's indentation rules."
+  (interactive "r")
+  (save-excursion
+      (nxml-mode)
+      (goto-char begin)
+      (while (search-forward-regexp "\>[ \\t]*\<" nil t)
+        (backward-char) (insert "\n"))
+      (indent-region begin end))
+    (message "Ah, much better!"))
