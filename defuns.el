efaa1ffe9fe373b5a07d220d864d0e1e9e541657
Mauricio Aldazosa Mariaca <mauricio.aldazosa@ciencias.unam.mx>
2013-09-21 12:19:22 -0500

    Adds eval-and-replace.

--- defuns.el
+++ defuns.el
@@ -67,4 +67,14 @@ by using nxml's indentation rules."
   (interactive)
   (insert-comment-annotation "TODO"))
 
+(defun eval-and-replace ()
+  "Replace the preceding sexp with its value."
+  (interactive)
+  (backward-kill-sexp)
+  (condition-case nil
+      (prin1 (eval (read (current-kill 0)))
+             (current-buffer))
+    (error (message "Invalid expression")
+           (insert (current-kill 0)))))
+
 (provide 'defuns)
