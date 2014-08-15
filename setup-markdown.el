06493df740aea77b9be268a200765a15f2c30495
Mauricio Aldazosa Mariaca <mauricio.aldazosa@ciencias.unam.mx>
2014-08-15 12:14:27 -0500

    Turn on visual-line and flyspell for markdown files.

--- setup-markdown.el
+++ setup-markdown.el
@@ -1,3 +1,9 @@
 (setq auto-mode-alist (append '(("\\.md$" . markdown-mode))
                               auto-mode-alist))
+
+(add-hook 'markdown-mode-hook
+          (lambda ()
+            (visual-line-mode t)
+            (flyspell-mode t)))
+
 (provide 'setup-markdown)
