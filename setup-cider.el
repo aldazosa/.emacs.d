6c3bd5e54d40ac897b68cb6e17eb293d05dd67e4
Mauricio Aldazosa Mariaca <mauricio.aldazosa@ciencias.unam.mx>
2014-09-30 21:06:04 -0500

    Adds a function to send a form to the repl but stay in the clj buffer.

--- setup-cider.el
+++ setup-cider.el
@@ -12,14 +12,21 @@
 (setq cider-server-command "lein trampoline repl :headless")
 (setq cider-repl-print-length 42)
 
+(defun my-cider-insert-defun-in-repl ()
+  (interactive)
+  (cider-insert-defun-in-repl 1)
+  (cider-switch-to-last-clojure-buffer))
+
 (eval-after-load 'cider
                  '(progn
                    (setq lisp-indent-function 'clojure-indent-function)
                    ;; (define-key cider-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc)
-                   (define-key cider-mode-map (kbd "C-c C-f") 'cider-eval-expression-at-point-in-repl)))
+                   (define-key cider-mode-map (kbd "C-c M-f") 'cider-pprint-eval-defun-at-point)
+                   (define-key cider-mode-map (kbd "C-c C-f") 'my-cider-insert-defun-in-repl)))
 
 ;; When developing leiningen plugins things we cant use trampoline
 ;; (setq nrepl-server-command "lein repl :headless")
 
 (setq cider-repl-use-clojure-font-lock t)
+
 (provide 'setup-cider)
